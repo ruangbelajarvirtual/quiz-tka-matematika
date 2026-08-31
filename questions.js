b6ae0b5c8736c446c952cf4c8a673d612ca3bca7
ruangbelajarvirtual <rusli.bima@gmail.com>
2026-08-31 23:55:31 +0700

    Create questions.js with math questions
    Added a set of math questions with multiple-choice options and answers.

---
+++ questions.js
@@ -0,0 +1,66 @@
+const questions = [
+  {
+    question: "Sebuah mobil menempuh setengah jarak perjalanan dengan kecepatan 60 km/jam dan setengah jarak berikutnya dengan kecepatan 90 km/jam. Kecepatan rata-rata mobil adalah ....",
+    image: "",
+    options: [
+      "70 km/jam",
+      "72 km/jam",
+      "75 km/jam",
+      "78 km/jam",
+      "80 km/jam"
+    ],
+    answer: 1
+  },
+
+  {
+    question: "Sebuah segitiga siku-siku memiliki panjang sisi miring 13 cm dan salah satu sisi siku-sikunya 5 cm. Luas segitiga tersebut adalah ....",
+    image: "",
+    options: [
+      "24 cm²",
+      "28 cm²",
+      "30 cm²",
+      "32 cm²",
+      "36 cm²"
+    ],
+    answer: 2
+  },
+
+  {
+    question: "Diketahui suatu barisan aritmetika memiliki suku pertama 5 dan beda 3. Nilai suku ke-10 adalah ....",
+    image: "",
+    options: [
+      "27",
+      "30",
+      "32",
+      "35",
+      "38"
+    ],
+    answer: 2
+  },
+
+  {
+    question: "Nilai dari √144 + √81 adalah ....",
+    image: "",
+    options: [
+      "19",
+      "20",
+      "21",
+      "22",
+      "23"
+    ],
+    answer: 2
+  },
+
+  {
+    question: "Sebuah persegi memiliki panjang sisi 12 cm. Luas persegi tersebut adalah ....",
+    image: "",
+    options: [
+      "124 cm²",
+      "132 cm²",
+      "140 cm²",
+      "144 cm²",
+      "156 cm²"
+    ],
+    answer: 3
+  }
+];
